843207d28d69ffaa1c01fdc8ad9c38c032e3d1df
AB-28 <alisha2828@gmail.com>
2022-06-26 23:28:01 +0530

    updated homepage url

--- src/NameCard/NameCard.js
+++ src/NameCard/NameCard.js
@@ -4,7 +4,7 @@ import './NameCard.css';
 const nameCheapUrl = "https://www.namecheap.com/domains/registration/results/?domain=%27%3B";
 const NameCard = ({ suggestedNames }) => {
     return (
-        <a target="_blank" rel="noopener noreferrer" className="card-link" href={`${nameCheapUrl}${suggestedNames}`}>
+        <a target="_blank" rel="noreferrer" className="card-link" href={`${nameCheapUrl}${suggestedNames}`}>
             <div className="result-name-card">
                 <p className="result-name">{suggestedNames}</p>
             </div>
