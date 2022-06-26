480f257b581eba4b1e782925350d679a2b7332ad
AB-28 <alisha2828@gmail.com>
2022-06-26 23:35:02 +0530

    Updates

---
+++ precache-manifest.93e23f061c2aaed1ca43730fa264700b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96c67f82e59a2d80dc7a",
+    "url": "/nameIt/static/css/main.abca87ef.chunk.css"
+  },
+  {
+    "revision": "96c67f82e59a2d80dc7a",
+    "url": "/nameIt/static/js/main.96c67f82.chunk.js"
+  },
+  {
+    "revision": "a068c84960762fe7f57d",
+    "url": "/nameIt/static/js/1.a068c849.chunk.js"
+  },
+  {
+    "revision": "4ec7e05fba9611615b2b",
+    "url": "/nameIt/static/js/2.4ec7e05f.chunk.js"
+  },
+  {
+    "revision": "22bdc3db03814176d10e",
+    "url": "/nameIt/static/js/runtime~main.22bdc3db.js"
+  },
+  {
+    "revision": "86ff4eb70387948d722b7fbad5511a3f",
+    "url": "/nameIt/index.html"
+  }
+];
